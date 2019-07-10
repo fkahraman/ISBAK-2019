b36d23f319a4a39958c227798133d60620c9f057
Fatih Kahraman <fatih.khrmn@hotmail.com>
2019-07-10 22:04:53 +0300

    Park Data, Algorithm, Pandas Database and Graphic
    Use python/pandas and python/pylab

---
+++ ClioTest/Test4_S4/showGrap4.py
@@ -0,0 +1,102 @@
+from pylab import *
+import pandas as pd
+
+parkData = []
+
+if __name__ == '__main__':
+
+    #   #   #   Verilerin Okunmasu  #   #   #
+
+    DATAEMA = pd.read_csv('EMAValue.txt', header=None, names=['EMA'])
+    DATASMA = pd.read_csv('SMAValue.txt', header=None, names=['SMA'])
+    DATASensor = pd.read_csv('SensorValue.txt', header=None, names=['Sensor'])
+    DATASapma = pd.read_csv('SapmaValue.txt', header=None, names=['Sapma'])
+    DATASapmaSMA = pd.read_csv('SapmaSMAValue.txt', header=None, names=['SapmaSMA'])
+
+    #   #   #   PARK ALGORİTMASI #  #   #
+
+    ct = 0
+    parkSayisi = 0
+
+    park = False
+    parkcikisbekleme = False
+
+    while(ct < 1295):
+
+        if parkcikisbekleme == False:
+
+            if park == False:
+
+                if (DATASapmaSMA.SapmaSMA[ct] > DATASapma.Sapma[ct]):
+
+                    if (DATASapmaSMA.SapmaSMA[ct] > 50):
+
+                        if (DATASapmaSMA.SapmaSMA[ct] < DATASMA.SMA[ct]):
+
+                            if(DATASapmaSMA.SapmaSMA[ct] < DATASensor.Sensor[ct]):
+
+                                parkSayisi += 1
+                                parkData.append(ct)
+                                park = True
+
+            else:
+
+                if (DATASapmaSMA.SapmaSMA[ct] > DATASapma.Sapma[ct]):
+
+                    if (DATASapmaSMA.SapmaSMA[ct] > 100):
+
+                        if (DATASapmaSMA.SapmaSMA[ct] > DATASMA.SMA[ct]):
+
+                            park = False
+                            parkcikisbekleme = True
+
+        else:
+
+            if(DATASapmaSMA.SapmaSMA[ct] < 50):
+
+                parkcikisbekleme = False
+
+        ct += 1
+
+    print("Ct:" , ct)
+    print("Park Sayısı: ",parkSayisi)
+    print(parkData)
+
+    #   #   #
+
+    parkDataY = []
+
+    sayac = 0
+
+    #   #   #   Grafiksel Kısım   #   #   #
+
+    grid = True
+    title("Araç Park Sistemi Giriş-Çıkış Grafiği")
+    xlabel("Time")
+    ylabel("Giriş-Çıkış")
+
+    x = arange(0, 1295, 1)
+    y = array(DATASensor[0:1295])
+
+    #xS = arange(0, 1295, 1)
+    #yS = array(DATAEMA[0:1295])
+
+    x_sma = arange(0, 1295, 1)
+    y_sma = array(DATASMA[0:1295])
+
+    x_sapma = arange(0,1295,1)
+    y_sapma = array(DATASapma[0:1295])
+
+    x_sapmaSMA = arange (0,1295,1)
+    y_sapmaSMA = array(DATASapmaSMA[0:1295])
+
+    plot(x, y, "-o",label='Sensör Grafiği')
+    #plot(xS, yS, "-o",label="EMA Grafiği")
+    plot(x_sma, y_sma, "-o",label="SMA Grafiği")
+    plot(x_sapma, y_sapma, "-o",label="Standart Sapma")
+    plot(x_sapmaSMA, y_sapmaSMA, "-o", label="Sapma SMA")
+
+    legend()
+    show()
+
+
---
+++ ClioTest/Test4_S5/ClioTest4.py
@@ -0,0 +1,144 @@
+from pylab import *
+import pandas as pd
+from math import sqrt
+
+def stdSapma(startDay, stdCounter, data):
+
+    if(stdCounter > startDay):
+        return
+
+    toplam = 0
+    karetoplam = 0
+    ct = 0
+
+    while(stdCounter > 0):
+
+        toplam = toplam + data[startDay - stdCounter]
+        stdCounter -= 1
+        ct += 1
+
+    ortalama = toplam/ct
+
+    while(ct > 0):
+
+        karetoplam = (data[startDay-ct] - ortalama)**2 + karetoplam
+        ct -= 1
+        stdCounter += 1
+
+    return sqrt(karetoplam/(stdCounter-1))
+
+def SMA(startDay, smaCounter, data):
+
+    toplam = 0
+    startCounter = smaCounter
+
+    while(smaCounter > 0):
+
+        smaValue = int(data[startDay - smaCounter])
+        toplam = toplam + smaValue
+        smaCounter -= 1
+
+    return int(toplam / startCounter)
+
+def EMA(startDay, emaCounter, data):
+
+    k1 = 1 #ilk indis
+    k2 = 1 #ikinci indis
+
+    ema = int(data[startDay - emaCounter])
+
+    emaValue = ema
+    emaCounter -= 1
+
+    while(emaCounter > 1):
+
+        emaValue = (2/(2+k1)) * data[startDay - emaCounter] + ((0+k2)/(2+k2)) * emaValue
+
+        k1 += 1
+        k2 += 1
+        emaCounter -= 1
+
+    return emaValue
+
+
+if __name__ == '__main__':
+
+    emaDatalari = []
+    smaDatalari = []
+    sapmaDatalari = []
+    sapmaSMA = []
+
+    df = pd.read_excel('Clio test grafikleriPy.xlsx', sheet_name='TEST4', header=None)
+
+    #aracDurum = df[8]   # Şahsi değerler
+    sensorToplam = df[13]    # 3 eksen toplam değerler
+
+    sensorToplam = sensorToplam[2:1318]
+
+    day = 15
+
+    for j in range(1300):
+
+        emaDatalari.append(EMA(day, 5, sensorToplam))
+        day += 1
+
+    day = 15
+
+    for h in range(1300):
+
+        smaDatalari.append(SMA(day, 5, sensorToplam))
+        day += 1
+
+    day = 15
+
+    for n in range(1300):
+
+        sapmaDatalari.append(stdSapma(day, 5, sensorToplam))
+        day += 1
+
+    sensorToplam = sensorToplam[15:1300]
+
+    day = 10
+
+    for i in range(1290):
+
+        sapmaSMA.append(SMA(day, 3, sapmaDatalari))
+        day += 1
+
+    dizi = [0,0,0,0,0,0,0,0,0,0]
+
+    sapmaSMA = dizi + sapmaSMA
+    print(sapmaSMA)
+
+    # # # Sensör Degerlerinden EMA,SMA degerlerinin çıkarılması ve kaydedilmesi # # #
+
+    DataSetSensor = list(zip(sensorToplam))
+    df = pd.DataFrame(data=DataSetSensor, columns=None)
+    df.to_csv('SensorValue.txt', index=False, header=False)
+    Location = r'C:\Users\fatih\PycharmProjects\untitled\AFTER ISBAK 2019\ClioTest\Test4_S5\SensorValue.txt'
+    #DATASensor = pd.read_csv('SensorValue.txt', header=None, names=['Sensor_Degerleri'])
+
+    DataSetEMA = list(zip(emaDatalari))
+    df = pd.DataFrame(data=DataSetEMA, columns=None)
+    df.to_csv('EMAValue.txt', index=False, header=False)
+    Location2 = r'C:\Users\fatih\PycharmProjects\untitled\AFTER ISBAK 2019\ClioTest\Test4_S5\EMAValue.txt'
+    #DATAEMA = pd.read_csv('EMAValue.txt', header=None, names=['EMA_Degerleri'])
+
+    DataSetSMA = list(zip(smaDatalari))
+    df = pd.DataFrame(data=DataSetSMA, columns=None)
+    df.to_csv('SMAValue.txt', index=False, header=False)
+    Location3 = r'C:\Users\fatih\PycharmProjects\untitled\AFTER ISBAK 2019\ClioTest\Test4_S5\SMAValue.txt'
+    #DATASMA = pd.read_csv('SMAValue.txt', header=None, names=['SMA_Degerleri'])
+
+    DataSetSapma = list(zip(sapmaDatalari))
+    df = pd.DataFrame(data=DataSetSapma, columns=None)
+    df.to_csv('SapmaValue.txt', index=False, header=False)
+    Location4 = r'C:\Users\fatih\PycharmProjects\untitled\AFTER ISBAK 2019\ClioTest\Test4_S5\SapmaValue.txt'
+
+    DataSetSapmaSMA = list(zip(sapmaSMA))
+    df = pd.DataFrame(data=DataSetSapmaSMA, columns=None)
+    df.to_csv('SapmaSMAValue.txt', index=False, header=False)
+    Location5 = r'C:\Users\fatih\PycharmProjects\untitled\AFTER ISBAK 2019\ClioTest\Test4_S5\SapmaSMAValue.txt'
+
+
+
